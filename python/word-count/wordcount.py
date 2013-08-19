0efee88de299ee9cdc4c91728943bf0cc195ed7b
wusher <willusher+github@gmail.com>
2013-08-18 17:10:12 -0700

    changed from using default dict + reduce to counter

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,15 +1,12 @@
 import re
-from collections import defaultdict
+from collections import Counter
+
 class Phrase:
   def __init__(self, sentence):
     self.__sentence = sentence.lower()
 
   def word_count(self):
-    return reduce(self.__increment_word, self.__words(), defaultdict(lambda: 0, {}))
-
-  def __increment_word(self, counts, word):
-    counts[word] += 1 
-    return counts
+    return Counter(self.__words())
 
   def __words(self):
     return re.findall("\w+", self.__sentence)
